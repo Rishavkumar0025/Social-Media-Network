9da397694627715df68db00521faaca0c4c5e695
Rishav Kumar <111872595+Rishavkumar0025@users.noreply.github.com>
2023-08-29 00:14:38 +0530

    Update Server.js
    Added various comments to facilitate reading and understanding the code.

--- Server.js
+++ Server.js
@@ -1,11 +1,14 @@
 "use strict";
 const http = require("http");
 class server {
+    // This function will parse the req object. This function is responsible for extracting imp. info. from the req object. It will return all the imp. info. as an object. The req object is huge in 
+    // size and because of that if we pass the whole req object it will consume a lot of data and time so we would extract imp. info. from the req object and pass only that imp. info. to save space and time.
     parse(req) {
         req.url = req.url.replace('/', '');
         let url = req.url.split("/");
-        return { 'url': url };
+        return { 'url': url }; // This object contains all the imp. info.
     }
+    // This function will route the request to the specific handler function.
     route(data) {
         let reqTable = { 'S': 'Static' };
         if (reqTable[data['url'][0]]) {
@@ -18,6 +21,7 @@ class server {
             return (new Server(data));
         }
     }
+    // This function will give the response to the client.
     respond(res, response) {
         res.writeHead(response[0], response[1]);
         res.end(response[2]);
